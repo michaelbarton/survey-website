5f90bbf8a764639b07a767c83800988f36eba3fb
Michael Barton <mail@michaelbarton.me.uk>
2011-11-22 14:22:00 -0500

    Add analytics

--- lib/google_analytics.rb
+++ lib/google_analytics.rb
@@ -1,15 +1,18 @@
 def google_analytics(token)
   <<-EOS
-  <script>
-      var _gaq = [['_setAccount', '#{token}'], ['_trackPageview']];
-      (function(d, t) {
-          var g = d.createElement(t),
-              s = d.getElementsByTagName(t)[0];
-          g.async = true;
-          g.src = '//www.google-analytics.com/ga.js';
-          s.parentNode.insertBefore(g, s);
-      })(document, 'script');
-  </script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '#{token}']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
   EOS
 end
 
